6fb44097e22e4615becc20a39750ea8290f903d0
Dexter Sampson <dexter1.sampson@gmail.com>
2020-02-28 22:13:48 -0500

    conditions added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,31 +23,92 @@ var losses= 0
 
 var totalScore= 0
 
-$("#wins").text("Wins:" + wins);
+$("#wins").text(wins);
 
-$("#losses").text("Losses:" + losses);
+$("#losses").text(losses);
+
+//reset
+function reset(){
+    targetNumber= Math.floor(Math.random ()*101)+ 19;
+    $("#targetNumber").text(targetNumber);
+    blueCrystal = Math.floor(Math.random()*11)+ 1;
+
+    purpleCrystal = Math.floor(Math.random()*11)+ 1;
+
+    silverCrystal = Math.floor(Math.random()*11)+ 1;
+
+    yellowCrystal = Math.floor(Math.random ()*11)+ 1;
+    totalScore=0;
+    $("#totalScore").text(totalScore);
+
+}
 
 //click function for each crystal
 
 $("#blueCrystal").on('click', function(){
     totalScore= totalScore + blueCrystal;
     $("#totalScore").text(totalScore)
+    if (totalScore == targetNumber){
+        wins++;
+        $("#wins").text(wins);
+        reset()
+    }
+    else if (totalScore > targetNumber){
+        losses++;
+        $("#losses").text(losses)
+        reset()
+    }
 });
 
 $("#purpleCrystal").on('click', function(){
     totalScore= totalScore + purpleCrystal;
     $("#totalScore").text(totalScore)
+    if (totalScore == targetNumber){
+        wins++;
+        $("#wins").text(wins);
+        reset()
+    }
+    else if (totalScore > targetNumber){
+        losses++;
+        $("#losses").text(losses)
+        reset()
+    }
 });
 
 $("#silverCrystal").on('click', function(){
     totalScore= totalScore + silverCrystal;
     $("#totalScore").text(totalScore)
+    if (totalScore == targetNumber){
+        wins++;
+        $("#wins").text(wins);
+        reset()
+    }
+    else if (totalScore > targetNumber){
+        losses++;
+        $("#losses").text(losses)
+        reset()
+    }
 });
 
 $("#yellowCrystal").on('click', function(){
     totalScore= totalScore + yellowCrystal;
     $("#totalScore").text(totalScore)
+    if (totalScore == targetNumber){
+        wins++;
+        $("#wins").text(wins);
+        reset()
+    }
+    else if (totalScore > targetNumber){
+        losses++;
+        $("#losses").text(losses)
+        reset()
+    }
 });
+
+
+
+
+
 
 
 
